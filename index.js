89f343af417b84011fa5d34f36a00cac30f6a00a
janeishahu <janeishahu@aol.com>
2021-07-25 20:49:14 -0400

    set up the function for the null test

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function calculate(inputs) {
     // replace all nulls with 0
     if (item === null) {
       item = 0
+      // what to do when the operator is encountered. 0 will be placed into (equal to) input2 since it comes after the operator. If it is not encountered, then the 0 will go into (equal) input1. 
+    } if (operator) {
+      input2 = 0
+    } else {
+      input1 = 0
     }
   })
 }
