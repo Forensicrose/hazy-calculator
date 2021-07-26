146ba63f9f8beef261c1108714e70bca5701c423
janeishahu <janeishahu@aol.com>
2021-07-25 20:40:01 -0400

    set up the 'calculate' function, pass in 'inputs' parameter, defined 'input1', 'input2' and 'operator' variables. Added comments for each of the steps.

---
+++ index.js
@@ -0,0 +1,15 @@
+// set up calculate function and pass in the parameter inputs
+
+function calculate(inputs) {
+  // define variables needed for the function 
+  let input1; let input2; let operator
+
+  // set up forEach loop since using arrays
+
+  inputs.forEach((item) => {
+    // replace all nulls with 0
+    if (item === null) {
+      item = 0
+    }
+  })
+}
